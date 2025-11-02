ba825c781ef967f0552e0e4a760ca1f223b3936c
Abiel Ortega <abielortega90@gmail.com>
2025-11-02 01:06:02 +0000

    CHORE: Updated some of the layout stuff for the titles so that it is more responsive for different screen sizes

--- src/App.tsx
+++ src/App.tsx
@@ -5,125 +5,135 @@ import { useState } from "react";
 import AccordionChapters from "./Components/AccordionChapters.js";
 
 const pageText = {
-	heading: "React Key Concepts",
+  heading: "React Key Concepts",
 };
 
 function App() {
-	const [message, setMessage] = useState(false);
+  const [message, setMessage] = useState(false);
 
-	function handleMessage() {
-		setMessage(!message);
-	}
+  function handleMessage() {
+    setMessage(!message);
+  }
 
-	// useEffect(() => {
-	// 	const timer = setTimeout(() => {
-	// 		setMessage(true);
-	// 	}, 1000);
+  // useEffect(() => {
+  // 	const timer = setTimeout(() => {
+  // 		setMessage(true);
+  // 	}, 1000);
 
-	// 	return () => clearTimeout(timer);
-	// }, []);
+  // 	return () => clearTimeout(timer);
+  // }, []);
 
-	// useEffect(() => {
-	// 	const timer = setTimeout(() => {
-	// 		setMessage(false);
-	// 	}, 8000);
+  // useEffect(() => {
+  // 	const timer = setTimeout(() => {
+  // 		setMessage(false);
+  // 	}, 8000);
 
-	// 	return () => clearTimeout(timer);
-	// });
+  // 	return () => clearTimeout(timer);
+  // });
 
-	return (
-		<AnimatePageTransition type="landingPage">
-			<div className="w-screen h-screen bg-primary flex flex-col items-center justify-center text-center text-foreground relative">
-				<h1 className="text-[248px] thunder-bold font-black text-background">
-					{pageText.heading.toUpperCase()}
-				</h1>
-				<h2 className="text-2xl font-bold text-foreground/70 mb-4">
-					An in-depth guide to React's core features
-				</h2>
+  return (
+    <AnimatePageTransition type="landingPage">
+      <div className="w-screen h-screen bg-primary flex flex-col items-center justify-center text-center text-foreground relative">
+        <h1 className="text-[150px] text-shadow-lg leading-30 lg:text-[248px] thunder-bold font-black text-background">
+          {pageText.heading.toUpperCase()}
+        </h1>
+        <h2 className="text-2xl font-bold text-foreground/70 mb-4">
+          An in-depth guide to React's core features
+        </h2>
 
-				{/* Message animation */}
-				<motion.div
-					className="bg-background text-s rounded-full bottom-80 overflow-hidden flex items-center justify-center cursor-pointer absolute"
-					style={{
-						width: message ? "1000px" : "100px",
-						height: message ? "100px" : "100px",
-					}}
-					animate={{
-						width: message ? "1000px" : "100px",
-						height: message ? "100px" : "100px",
-					}}
-					transition={{ duration: 0.5, delay: 0.5, ease: "easeInOut" }}
-					onClick={handleMessage}
-				>
-					<AnimatePresence mode="wait">
-						{message ? (
-							<motion.div
-								key="message"
-								className="overflow-hidden px-20 py-10 text-left text-foreground"
-								initial={{ opacity: 0 }}
-								animate={{ opacity: 1 }}
-								exit={{ opacity: 0 }}
-								transition={{ duration: 0.5, ease: "linear" }}
-							>
-								<p>
-									This site is inspired by the book{" "}
-									<strong className="text-primary">
-										"React Key Concepts" by Maximillian Schwarezmuller
-									</strong>
-									. It serves as a companion to the book, providing interactive
-									examples and exercises to help reinforce the concepts covered
-									in each chapter.
-								</p>
-							</motion.div>
-						) : (
-							<motion.div className="bg-transparent border-0 relative flex items-center justify-center">
-								<motion.div
-									key="icon"
-									className="bg-background border-0"
-									initial={{ opacity: 0 }}
-									animate={{ opacity: 1 }}
-									exit={{ opacity: 0 }}
-									transition={{ duration: 1, ease: "linear" }}
-								>
-									<MessageSquareMore className="bg-background scale-x-[-1] animate-caret-blink" />
-								</motion.div>
-							</motion.div>
-						)}
-					</AnimatePresence>
-				</motion.div>
-				<div>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						className="lucide lucide-arrow-down-icon lucide-arrow-down animate-bounce scale-400 absolute bottom-20 right-20 text-accent"
-					>
-						<path d="M12 5v14" />
-						<path d="m19 12-7 7-7-7" />
-					</svg>
-				</div>
-			</div>
+        {/* Message animation */}
+        <motion.div
+          className="max-w-150 bg-background text-xs font-light lg:text-s rounded-full bottom-30 lg:bottom-80 overflow-hidden flex items-center justify-center cursor-pointer absolute"
+          style={{
+            width: message ? "1000px" : "100px",
+            height: message ? "100px" : "100px",
+          }}
+          animate={{
+            width: message ? "1000px" : "100px",
+            height: message ? "100px" : "100px",
+          }}
+          transition={{ duration: 0.5, delay: 0.5, ease: "easeInOut" }}
+          onClick={handleMessage}
+        >
+          <AnimatePresence mode="wait">
+            {message ? (
+              <motion.div
+                key="message"
+                className="overflow-hidden px-20 py-10 text-left text-foreground"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5, ease: "linear" }}
+              >
+                <p>
+                  This site is inspired by the book{" "}
+                  <strong className="text-primary">
+                    "React Key Concepts" by Maximillian Schwarezmuller
+                  </strong>
+                  . It serves as a companion to the book, providing interactive
+                  examples and exercises to help reinforce the concepts covered
+                  in each chapter.
+                </p>
+              </motion.div>
+            ) : (
+              <motion.div className="bg-transparent border-0 relative flex items-center justify-center">
+                <motion.div
+                  key="icon"
+                  className="bg-background border-0"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 1, ease: "linear" }}
+                >
+                  <MessageSquareMore className="bg-background scale-x-[-1] animate-caret-blink" />
+                </motion.div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </motion.div>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-arrow-down-icon lucide-arrow-down animate-bounce scale-200 lg:scale-400 absolute bottom-15 lg:bottom-20 right-20 text-accent"
+          >
+            <path d="M12 5v14" />
+            <path d="m19 12-7 7-7-7" />
+          </svg>
+        </div>
+      </div>
 
-			{/* Link to Events Page */}
-			<div className="w-full h-screen bg-[#181b21]">
-				<div className="p-20">
-					<div className="bg-background/10">
-					{/* TODO: Make backgroubnd animation on scroll. */}
-						<h2 className="thunder-bold text-secondary-foreground text-[240px] leading-50 relative bottom-10 right-5">
-							EXPLORE ALL EXERCISES AND CHAPTERS
-						</h2>
-					</div>
-				</div>
-				<AccordionChapters />
-			</div>
-		</AnimatePageTransition>
-	);
+      {/* Chapter main page section. */}
+      <div className="w-full bg-[#181b21]">
+        <div className="p-20">
+          <div className="bg-background/10">
+            {/* TODO: Make backgroubnd animation on scroll. */}
+            <h2 className="thunder-bold text-secondary-foreground leading-30 text-[150px] lg:text-[240px] lg:leading-50 relative bottom-10 right-5">
+              EXPLORE ALL EXERCISES AND CHAPTERS
+            </h2>
+          </div>
+        </div>
+        <AccordionChapters />
+      </div>
+
+      <footer className="w-full h-screen bg-background p-20 flex items-center justify-center">
+        <h2 className="thunder-bold leading-20 text-[100px] lg:text-[180px] lg:leading-50">
+          DESIGNED AND BUILT BY{" "}
+          <strong className="text-primary">
+            <br />
+            ABIEL ORTEGA.
+          </strong>
+        </h2>{" "}
+      </footer>
+    </AnimatePageTransition>
+  );
 }
 
 export default App;
--- src/Components/AccordionChapters.tsx
+++ src/Components/AccordionChapters.tsx
@@ -1,71 +1,73 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button.js";
 import {
-	Accordion,
-	AccordionItem,
-	AccordionContent,
-	AccordionTrigger,
+  Accordion,
+  AccordionItem,
+  AccordionContent,
+  AccordionTrigger,
 } from "./ui/accordion.js";
 import { useState } from "react";
 
 const chapterData = [
-	{
-		title: "Chapter 1: Introduction to React",
-		summary:
-			"Get started with the basics of React, including components, JSX, and props.",
-        chapter: 1,
-        chapterLink: "/chapter-1-introduction-to-react"
-	},
-	{
-		title: "Chapter 2: Learning more on React",
-		summary:
-			"Here is another summuary for chapter 2 to see how this looks like in the accordion component.",
-        chapter: 2,
-        chapterLink: "/chapter-2-introduction-to-react"
-	},
+  {
+    title: "Chapter 1",
+    subTitle: "Introduction to React",
+    summary:
+      "Get started with the basics of React including components, JSX, and props.",
+    chapter: 1,
+    chapterLink: "/chapter-1-introduction-to-react",
+  },
+  {
+    title: "Chapter 2",
+    subTitle: "Learning more on React",
+    summary:
+      "Here is another summuary for chapter 2 to see how this looks like in the accordion component.",
+    chapter: 2,
+    chapterLink: "/chapter-2-introduction-to-react",
+  },
 ];
 const AccordionChapters = () => {
-    const [background, setBackground ]= useState('')
+  const [background, setBackground] = useState("");
 
-    //TODO: Move this to be a separte component to be able to manage the separate change in the background color or see if you are able to manage it on it's own here.
+  //TODO: Move this to be a separte component to be able to manage the separate change in the background color or see if you are able to manage it on it's own here.
 
-    // function handleBackgroundChange() {
-    //     // Logic to change background based on chapter title
-    //     if (background === '') {
-    //         setBackground('oklch(0.6832 0.1769 246.9929)');
-    //         return;
-    //     }   else{
-    //         setBackground('');
-    //         return;
-    //     }
-    // }
+  // function handleBackgroundChange() {
+  //     // Logic to change background based on chapter title
+  //     if (background === '') {
+  //         setBackground('oklch(0.6832 0.1769 246.9929)');
+  //         return;
+  //     }   else{
+  //         setBackground('');
+  //         return;
+  //     }
+  // }
 
-	return (
-		<div>
-			{/* Make this area bigger so that it's like drop downs of the chapters with short summariez and then reach out to the author to maybe even just read over it if he likes it! */}
-			<Accordion
-				type="single"
-				collapsible
-				className="w-full"
-                >
-                {chapterData.map((chapter) => (
-				<AccordionItem value={`item-${chapter.chapter}`} className="border-b">
-					<AccordionTrigger className="rounded-none border-b text-accent hover:bg-accent-foreground px-20 flex items-center" style={{backgroundColor: background}}>
-						<h2 className="font-normal text-8xl tracking-tighter">{chapter.title.toUpperCase()}</h2>
-					</AccordionTrigger>
-					<AccordionContent className="px-25 py-10 bg-background">
-                        <p className="text-3xl mb-6">
-                            {chapter.summary}
-                        </p>
-						<Button asChild variant="secondary" size="lg">
-							<Link to="/events">OPEN CHAPTER</Link>
-						</Button>
-					</AccordionContent>
-				</AccordionItem>
-                ))}
-			</Accordion>
-		</div>
-	);
+  return (
+    <div>
+      {/* Make this area bigger so that it's like drop downs of the chapters with short summariez and then reach out to the author to maybe even just read over it if he likes it! */}
+      <Accordion type="single" collapsible className="w-full">
+        {chapterData.map((chapter) => (
+          <AccordionItem value={`item-${chapter.chapter}`} className="border-b">
+            <AccordionTrigger
+              className="rounded-none border-b text-accent hover:bg-accent-foreground px-20 flex items-center"
+              style={{ backgroundColor: background }}
+            >
+              <h2 className="font-black text-4xl lg:text-8xl text-secondary-foreground tracking-tighter">
+                {chapter.title.toUpperCase()}
+              </h2>
+            </AccordionTrigger>
+            <AccordionContent className="px-25 py-10 bg-background">
+              <h3 className="text-4xl font-black mb-5">{chapter.subTitle}</h3>
+              <p className="text-2xl mb-6">{chapter.summary}</p>
+              <Button asChild variant="secondary" size="lg">
+                <Link to="/events">OPEN CHAPTER</Link>
+              </Button>
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  );
 };
 
 export default AccordionChapters;
